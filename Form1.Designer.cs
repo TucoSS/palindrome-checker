01cda8c0b6cca48e20ecc79fd25f77c595213b68
TucoSS <79876992+TucoSS@users.noreply.github.com>
2022-04-05 18:18:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SDA_46290
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblPalindrome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxWord.Location = new System.Drawing.Point(110, 81);
+            this.textBoxWord.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(220, 38);
+            this.textBoxWord.TabIndex = 0;
+            this.textBoxWord.TextChanged += new System.EventHandler(this.textBoxWord_TextChanged);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(36, 33);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(386, 36);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Type in a word and check if it is a Palindrome:";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Javanese Text", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(153, 140);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(139, 36);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter Word";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblPalindrome
+            // 
+            this.lblPalindrome.AutoSize = true;
+            this.lblPalindrome.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalindrome.Location = new System.Drawing.Point(88, 220);
+            this.lblPalindrome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPalindrome.Name = "lblPalindrome";
+            this.lblPalindrome.Size = new System.Drawing.Size(0, 36);
+            this.lblPalindrome.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 285);
+            this.Controls.Add(this.lblPalindrome);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.textBoxWord);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "SDA_46290";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblPalindrome;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDA_46290
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            lblPalindrome.Text = textBoxWord.Text;
+            String word = textBoxWord.Text.ToLower();
+            bool isPalindrome = palindromeChecker(word);
+            
+            if (isPalindrome)
+            {
+                lblPalindrome.Text += " is a Palindrome.";
+            }
+            else
+            {
+                lblPalindrome.Text += " is not a Palindrome.";
+            }
+        }
+
+        private bool palindromeChecker(String word)
+        {
+            char[] letters = word.ToCharArray();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if(letters[letters.Length - 1 - i] == letters[i])
+                {
+                    continue;
+                } else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void textBoxWord_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
